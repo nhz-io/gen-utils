9484679824d809f6e2052a5b999ebd08ae0cd2f8
Ishi Ruy <dev@nhz.io>
2017-02-04 23:00:39 +0200

    reindent find.js

--- src/find.js
+++ src/find.js
@@ -1,13 +1,13 @@
 function* find(list, fn) {
-  let i = 0
-  
-  for (const item of list) {
-    if (fn.call(this, item, i, list)) {
-      return yield item
+    let i = 0
+
+    for (const item of list) {
+        if (fn.call(this, item, i, list)) {
+            return yield item
+        }
     }
-  }
 }
 
 function* findR(fn, list) {
-  yield* find(list, fn)
+    yield* find(list, fn)
 }
