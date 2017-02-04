b9c9a718adcc0d3e885fe92dd2b37e34861c1e2d
Ishi Ruy <dev@nhz.io>
2017-02-04 23:01:44 +0200

    reindent reject.js

--- src/reject.js
+++ src/reject.js
@@ -1,13 +1,13 @@
 function* reject(list, fn) {
-  let i = 0
-  
-  for (const item of list) {
-    if (!fn.call(this, item, i, list)) {
-      yield item
+    let i = 0
+
+    for (const item of list) {
+        if (!fn.call(this, item, i, list)) {
+            yield item
+        }
     }
-  }
 }
 
 function* rejectR(fn, list) {
-  yield* reject(list, fn)
+    yield* reject(list, fn)
 }
