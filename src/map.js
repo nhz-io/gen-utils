15b3bdc1baada1c9873113f4901c59bf6ea0ece4
Ishi Ruy <dev@nhz.io>
2017-02-04 23:01:16 +0200

    reindent map.js

--- src/map.js
+++ src/map.js
@@ -1,11 +1,11 @@
 function* map(list, fn) {
-  let i = 0
-  
-  for (const item of list) {
-    yield fn.call(this, item, i, list)
-  }
+    let i = 0
+
+    for (const item of list) {
+        yield fn.call(this, item, i, list)
+    }
 }
 
 function* mapR(fn, list) {
-  yield* map(list, fn)
+    yield* map(list, fn)
 }
