fc9f05bbdf5484cbd3a1b32e5ed85e7d7a3a9d72
Ishi Ruy <dev@nhz.io>
2017-02-04 22:59:48 +0200

    reindent filter.js

--- src/filter.js
+++ src/filter.js
@@ -1,13 +1,13 @@
 function* filter(list, fn) {
-  let i = 0
-  
-  for (const item of list) {
-    if (fn.call(this, item, i, list)) {
-      yield item
+    let i = 0
+
+    for (const item of list) {
+        if (fn.call(this, item, i, list)) {
+            yield item
+        }
     }
-  }
 }
 
 function* filterR(fn, list) {
-  yield* filter(list, fn)
+    yield* filter(list, fn)
 }
