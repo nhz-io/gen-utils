87b08764c0d901d986ba512cbe2969d36dc13363
Ishi Ruy <dev@nhz.io>
2017-02-05 19:57:49 +0200

    Update index.js

--- src/ast/index.js
+++ src/ast/index.js
@@ -1,26 +1,29 @@
-function* astNodes(start) {
+function* astNodes(start, parent) {
+    if (!start || typeof start !== 'object') {
+        return
+    }
+    
+    parent = parent || start._parent
+ 
     start._scope = start._scope || []
     
     if (start && start.type && start.range && start.range.length) {
         start._uid = Math.random().toString(36).slice(2)
         
-        if (start._parent && start._parent._scope) {
-            start._parent._scope.push(start._uid)
+        if (parent && parent._scope) {
+            parent._scope.push(start._uid)
         }
         
         start._scope.push(start._uid)
+        parent = start
         
         yield start
-    }
-    
-    if (!start || typeof start !== 'object') {
-        return
     }
     
     for (const value of values(start)) {
         if (value !== start._scope && value !== start._parent && value !== start.range) {
             value._scope = start._scope.slice()
-            value._parent = start
+            value._parent = parent
         
             yield* astNodes(value)
         }
