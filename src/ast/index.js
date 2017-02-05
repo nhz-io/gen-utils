9424fe5ebbee09be2edc389ecd0372f7b16c2656
Ishi Ruy <dev@nhz.io>
2017-02-05 23:18:32 +0200

    Update index.js

--- src/ast/index.js
+++ src/ast/index.js
@@ -66,3 +66,131 @@ function* astNodes(start, parent) {
         yield* astNodes(v, parent)
     }
 }
+
+function* providedIdentifiers(...list) {
+    if (list.length != 1) {
+        return yield* regen.call(this, providedIdentifiers, list)
+    }
+    
+    const [nodes] = list
+    
+    for (const node of nodes) {
+        if (node.type !== 'Identifier') {
+            continue
+        }
+        
+        const parent = node.parent
+        
+        switch(parent.type) {
+            case 'AssignmentExpression':
+                if (parent.left === node) {
+                    yield node
+                }
+                
+                break
+                
+            case 'VariableDeclarator':
+                if (parent.id === node) {
+                    yield node
+                }
+                
+                break
+            
+            default:
+                console.log(
+                    `>> Unhandled provided identifier parent type: ${parent.type}`
+                )
+                
+                break
+        }
+    }
+}
+
+function* providers(...list) {
+    for (const node of providedIdentifiers(...list)) {
+        const parent = node.parent
+        
+        switch(parent.type) {
+            case 'AssignmentExpression':
+                if (parent.parent.type === 'ExpressionStatement') {
+                    yield parent.parent
+                }
+                
+                break
+            
+            case 'VariableDeclarator':
+                if (parent.parent.type === 'VariableDeclaration') {
+                    yield parent.parent
+                }
+                
+                break
+            
+            default:
+                console.log(
+                    `>> Unhandled provider type: ${parent.type}`
+                )
+                
+                break
+        }
+    }
+}
+
+function* consumedIdentifiers(...list) {
+    if (list.length != 1) {
+        return yield* regen(consumedIdentifiers, list)
+    }
+    
+    const [nodes] = list
+    
+    for (const node of nodes) {
+        if (node.type !== 'Identifier') {
+            continue
+        }
+        
+        const parent = node.parent
+        
+        switch(parent.type) {
+            case 'VariableDeclarator':
+                if (parent.init === node) {
+                    yield node
+                }
+                
+                break
+
+            case 'MemberExpression': 
+                if (parent.object === node) {
+                    yield node
+                }
+                
+                break
+            
+            case 'CallExpression':
+                if (parent.arguments.includes(node)) {
+                    yield node
+                }
+                
+                break
+            
+            case 'AssignmentExpression':
+                if (parent.right === node) {
+                    yield node
+                }
+                
+                break
+            
+            case 'LogicalExpression':
+                if (parent.right === node) {
+                    yield node
+                }
+                
+                break
+                
+            default:
+                console.log(
+                    `>> Unhandled consumed identifier parent type: ${parent.type}`
+                )
+                
+                break
+        }
+    }
+}
