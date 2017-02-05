c682dbd702c00b6eec9174879efcb45e45985833
Ishi Ruy <dev@nhz.io>
2017-02-05 22:41:42 +0200

    Update uniq.js

--- src/iterable/uniq.js
+++ src/iterable/uniq.js
@@ -1,11 +1,13 @@
-function* uniq(list) {
+function* uniq(...list) {
     const seen = []
-
-    for (const item of list) {
-        if (seen.indexOf(item) === -1) {
-            seen.push(item)
-            
-            yield item
+    
+    for (const items of list) {
+        for (const item of items) {
+            if (seen.indexOf(item) === -1) {
+                seen.push(item)
+                
+                yield(item)
+            }
         }
     }
 }
