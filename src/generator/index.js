530059c52605a7a14b283964feda77b97eec2595
Ishi Ruy <dev@nhz.io>
2017-02-05 23:57:02 +0200

    Update index.js

--- src/generator/index.js
+++ src/generator/index.js
@@ -7,3 +7,54 @@ function* regen(gen, list) {
 function* regenR(list, gen) {
     yield* regen.call(this, gen, list)
 }
+
+function* repeat(...list) {
+    let n = list.pop()
+    const buffer = []
+    
+    for (const items of list) {
+        for (const item of items) {
+            buffer.push(item)
+            yield item
+        }
+    }
+    while (--n > 0) {
+        yield* buffer
+    }
+}
+
+function* repeatR(n, ...list) {
+    return yield* repeat(...list, n)
+}
+
+function* buffer(...list) {
+    const buff = list.pop()
+    
+    for (const items of list) {
+        for (const item of items) {
+            buff.push(item)
+            yield item
+        }
+    }
+}
+
+function* bufferR(buff, ...list) {
+    return yield* buffer(...list, buff)
+}
+    
+
+function* fork(...list)) {
+    const buff = []
+    let iterable = buffer(list.pop(), buff)
+    const gen = list.shift()
+    
+    yield* gen(iterable)
+    
+    for (const gen of list) {
+        yield* gen(buff)
+    }
+}
+
+function* forkR(iterable, ...list) {
+    return yield* fork(...list, iterable)
+}
