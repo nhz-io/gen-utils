2ee9cba0536bfd4abb5579360300a71061a2d1a9
Ishi Ruy <dev@nhz.io>
2017-02-05 19:42:28 +0200

    Update ast-nodes.js

--- src/ast/ast-nodes.js
+++ src/ast/ast-nodes.js
@@ -3,6 +3,11 @@ function* astNodes(start) {
     
     if (start && start.type && start.range && start.range.length) {
         start._uid = Math.random().toString(36).slice(2)
+        
+        if (start._parent && start._parent._scope) {
+            start._parent._scope.push(start._uid)
+        }
+        
         start._scope.push(start._uid)
         
         yield start
@@ -15,6 +20,7 @@ function* astNodes(start) {
     for (const value of values(start)) {
         if (value !== start._scope && value !== start.range) {
             value._scope = start._scope.slice()
+            value._parent = start
         
             yield* astNodes(value)
         }
