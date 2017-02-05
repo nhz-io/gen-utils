3ff12d33827b0efcf71ed5d029222a2ef6b46770
Ishi Ruy <dev@nhz.io>
2017-02-05 15:10:57 +0200

    added flatten

--- src/common/index.js
+++ src/common/index.js
@@ -26,6 +26,26 @@ function* findR(fn, list) {
     yield* find(list, fn)
 }
 
+function* flatten(...list) {
+    for (const item of list) {
+        if (item && typeof item[Symbol.iterator] === 'function') {
+            const list = item
+            
+            for (const item of list) {
+                if (item && typeof item [Symbol.iterator] === 'function') {
+                    yield* flatten(item)
+                }
+                else {
+                    yield item
+                }
+            }
+        }
+        else {
+            yield item
+        }
+    }
+}
+
 function* keys(target) {
     if (typeof target === 'undefined') {
         return yield* []
