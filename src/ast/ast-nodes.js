d1640f46c78fbc075695197616951d08ba8c1f6b
Ishi Ruy <dev@nhz.io>
2017-02-05 21:03:09 +0200

    Update ast-nodes.js

--- src/ast/ast-nodes.js
+++ src/ast/ast-nodes.js
@@ -1,28 +1,68 @@
-function* astNodes(start) {
-    start._scope = start._scope || []
-    
-    if (start && start.type && start.range && start.range.length) {
-        start._uid = Math.random().toString(36).slice(2)
-        
-        if (start._parent && start._parent._scope) {
-            start._parent._scope.push(start._uid)
-        }
-        
-        start._scope.push(start._uid)
-        
-        yield start
-    }
-    
+const scopedTypes = [
+    'Program',
+    'FunctionDeclaration',
+    'FunctionExpression',
+    'BlockStatement',
+]
+
+const rejectedKeys = [
+    'scope',
+    'parent',
+    'range',
+]
+
+function* astNodes(start, parent) {
     if (!start || typeof start !== 'object') {
         return
     }
-    
-    for (const value of values(start)) {
-        if (value !== start._scope && value !== start._parent && value !== start.range) {
-            value._scope = start._scope.slice()
-            value._parent = start
+
+    parent = parent || start.parent || {}
+
+    if (start.type && start.range && start.range.length) {
+        start.parent = parent
         
-            yield* astNodes(value)
+        if (scopedTypes.indexOf(start.type) !== -1) {
+            let _scope = []
+
+            Object.defineProperty(start, 'scope', {
+                get: () => {
+                    const scope = Array.from(
+                        uniq((start.parent.scope || []).concat(_scope))
+                    )
+
+                    scope.push = (...args) => {
+                        _scope.push(...args)
+                        _scope = Array.from(uniq(_scope))
+
+                        return _scope.length
+                    }
+
+                    return scope
+                },
+            })
         }
-    }    
+        else {
+            Object.defineProperty(start, 'scope', {
+                get: () => start.parent.scope || [],
+            })
+        }
+
+        yield start
+
+        parent = start
+        
+        if (start.parent.scope) {
+            start.parent.scope.push(start)
+        }
+
+        start.scope.push(start)
+    }
+
+    const ks = reject(keys(start), k => rejectedKeys.indexOf(k) !== -1 )
+
+    for (const k of ks) {
+        const v = start[k]
+
+        yield* astNodes(v, parent)
+    }
 }
