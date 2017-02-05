1817ddd81e1ee7e4711fc7f86f8b4f744cab7ee6
Ishi Ruy <dev@nhz.io>
2017-02-05 22:49:31 +0200

    Update index.js

--- src/iterable/index.js
+++ src/iterable/index.js
@@ -121,6 +121,38 @@ function* uniq(...list) {
     }
 }
 
+function* uniqBy(...list) {
+    const seen = []    
+    const diff = list.pop()
+    
+    if (typeof diff === 'function') {
+        for (const items of list) {
+            for (const item of items) {
+                if (diff.call(this, item, seen)) {
+                    yield item
+                }
+            }
+        }
+    }
+    else {
+        for (const items of list) {
+            for (const item of items) {
+                const key = item[diff]
+                
+                if (seen.indexOf(key) === -1) {
+                    seen.push(key)
+                    
+                    yield item
+                }
+            }
+        }
+    }
+}
+
+function* uniqByR(diff, ...list) {
+    return yield* uniqBy(...list, diff)
+}
+
 function* values(target) {
     if (typeof target === 'undefined') {
         return yield* []
