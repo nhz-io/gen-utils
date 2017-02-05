692fd1b36f6b3109ea9bb834bfb285511580bd15
Ishi Ruy <dev@nhz.io>
2017-02-06 00:21:07 +0200

    Update index.js

--- src/iterable/index.js
+++ src/iterable/index.js
@@ -172,3 +172,24 @@ function* values(target) {
         }
     }
 }
+
+function* recurring(...list) {
+    const seen = []
+    const yielded = []
+    
+    for (const items of list) {
+        for (const item of items) {
+            if (seen.indexOf(item) > -1) {
+                if (yielded.indexOf(item) < 0) {
+                    yielded.push(item)
+                    
+                    yield item
+                }
+            }
+            else {
+                seen.push(item)
+            }
+        }
+    }
+}
+            
